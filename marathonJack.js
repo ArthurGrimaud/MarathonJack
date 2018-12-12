018fafd08d0e8b9459c350c979d1eaf62540146c
ArthurGrimaud <arthur582strontium.gmail.com>
2018-12-12 11:11:25 +0100

    ajout fonction dans js

--- marathonJack.js
+++ marathonJack.js
@@ -1,22 +1,68 @@
-var dealerCardNb = 0;
-var yourCardNb = 0;
+
+
+var buttonCard = document.getElementById("buttonCard");
+var buttonStay = document.getElementById("buttonStay");
+
+var labelSumDealer = document.getElementById("sumDealerCards");
+var labelSumPlayer = document.getElementById("sumPlayerCards");
+
+var playerCardsSumValue = 0
+var dealerCardsSumValue = 0
 
 var cardList = [...Array(53).keys()];
-var pickedCard = [];  //Carte deja tirees
+var pickedCards = [];  //Carte deja tirees
+var nbCard = 52
 
+/* Tirer un carte et affichage  */
 
-
-function pickCard(cardList,pickedCard){
-  var rand = Math.floor(Math.random() * (53 - 0 + 1)) + 0;
-  while (pickedCard.includes(rand)){ // si la carte a deja ete tiree
-    rand = Math.floor(Math.random() * (53 - 0 + 1)) + 0; //une autre carte est choisie
+function drawCard(cardList,pickedCards){
+  var rand = Math.floor(Math.random() * (nbCard)) + 1;
+  while (pickedCards.includes(rand)){ // si la carte a deja ete tiree
+    rand = Math.floor(Math.random() * (nbCard)) + 1; //une autre carte est choisie
   }
-  pickedCard.push(rand); // on ajoute la carte tiree au carte deja tiree
+  pickedCards.push(rand); // on ajoute la carte tiree au carte deja tiree
   console.log(rand);
+  return rand;
 }
 
-function displayCard(cardValue,imageID){
-
-  str = cardValue.toString().concatenate(.BMP)
-  imageID.src = images[x];
+function createImg(path,div) {
+  var img = document.createElement('img');
+  img.src = path;
+  return img;
 }
+
+function displayNewCard(cardValue,div) { //arg : div : balise "div dans laquelle la carte est crée"
+  var path = "img/" + cardValue + ".BMP";
+  console.log(path);
+  var newImg = createImg(path);
+  var divJS = document.getElementById(div);
+  divJS.appendChild(newImg);
+  return cardValue
+}
+
+/* Listeners */
+
+function setupListener(){
+  console.log(buttonCard)
+  buttonCard.addEventListener("click",function(){card(playerCardsSumValue,cardList,pickedCards)});
+  buttonStay.addEventListener("click",function(){})
+}
+
+/* gameManager */
+
+function resetTurn(){
+  var playerCardsSumValue = 0
+  var dealerCardsSumValue = 0
+  document.getElementById("ddiv").innerHTML = "";
+  document.getElementById("ydiv").innerHTML = "";
+}
+
+function card(playerCardsSumValue,cardList,pickedCards){
+  playerCardsSumValue = playerCardsSumValue + displayNewCard(drawCard(cardList,pickedCards),"ydiv")
+  labelSumPlayer.innerHTML = playerCardsSumValue;
+}
+
+
+
+
+setupListener();
